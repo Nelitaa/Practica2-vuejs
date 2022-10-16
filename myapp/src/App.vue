fa399615c89b569c6f84725b628abb15338e7a1c
marianelamunozgutierrez@gmail.com <marianelamunozgutierrez@gmail.com>
2022-10-15 21:04:30 -0400

    Add footer

--- myapp/src/App.vue
+++ myapp/src/App.vue
@@ -1,10 +1,12 @@
 <template>
   <app-menu title="MegaDron"></app-menu>
   <router-view />
+  <footer style="background-color: Slategrey"></footer>
 </template>
 
 <script>
 import AppMenu from "@/components/AppMenu.vue";
+import AppFooter from "@/components/AppFooter.vue";
 export default {
   name: "app",
   data() {
@@ -15,6 +17,7 @@ export default {
   mounted() {},
   components: {
     AppMenu,
+    AppFooter,
   },
 };
 </script>
@@ -39,5 +42,47 @@ nav a {
 
 nav a.router-link-exact-active {
   color: #42b983;
+}
+
+footer {
+  text-align: center;
+  padding: 30px 10px;
+  margin-top: 50px;
+  min-height: 100px;
+}
+.color-box {
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  margin: 7px;
+  display: inline-block;
+}
+
+.clic {
+  cursor: pointer;
+}
+
+.quantity button {
+  border-radius: 50%;
+  display: inline-block;
+  width: 30px;
+}
+.quantity div {
+  text-align: center;
+  min-width: 30px;
+  display: inline-block;
+  font-weight: bold;
+}
+.buy-box {
+  margin: 20px;
+}
+.container {
+  margin-top: 50px;
+}
+.producto-relacionado-precio {
+  background: orangered;
+  color: white;
+  text-align: center;
+  padding: 10px;
 }
 </style>
---
+++ myapp/src/components/AppFooter.vue
@@ -0,0 +1,16 @@
+<template>
+  <footer></footer>
+</template>
+
+<script>
+export default {
+  name: "appFooter",
+  data() {
+    return {};
+  },
+  methods: {},
+  computed: {},
+  mounted() {},
+  components: {},
+};
+</script>
