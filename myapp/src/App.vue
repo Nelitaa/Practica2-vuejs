569a04e7ea728749f01f1087bd4721de7b6ef947
marianelamunozgutierrez@gmail.com <marianelamunozgutierrez@gmail.com>
2022-10-15 21:15:48 -0400

    Update style price

--- myapp/src/App.vue
+++ myapp/src/App.vue
@@ -50,39 +50,4 @@ footer {
   margin-top: 50px;
   min-height: 100px;
 }
-.color-box {
-  width: 40px;
-  height: 40px;
-  border-radius: 50%;
-  margin: 7px;
-  display: inline-block;
-}
-
-.clic {
-  cursor: pointer;
-}
-
-.quantity button {
-  border-radius: 50%;
-  display: inline-block;
-  width: 30px;
-}
-.quantity div {
-  text-align: center;
-  min-width: 30px;
-  display: inline-block;
-  font-weight: bold;
-}
-.buy-box {
-  margin: 20px;
-}
-.container {
-  margin-top: 50px;
-}
-.producto-relacionado-precio {
-  background: orangered;
-  color: white;
-  text-align: center;
-  padding: 10px;
-}
 </style>
--- myapp/src/components/HelloWorld.vue
+++ myapp/src/components/HelloWorld.vue
@@ -1,59 +1,96 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router" target="_blank" rel="noopener">router</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-vuex" target="_blank" rel="noopener">vuex</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  </div>
+
+  <div class="container">
+    <div class="row">
+      <h3>S6S Mini GPS Drones</h3>
+    </div>
+    <div class="row">
+      <div class="col-12 col-sm-6 col-md-4">
+        <img
+          src="https://ae01.alicdn.com/kf/S00eb0e55c14f47e2bbf828a92ecd5515U/S6S-Mini-GPS-Drones-150g-Drone-4K-Profesional-HD-Dual-Camera-5G-WIFI-FPV-Brushless-Folding.jpg_Q90.jpg_.webp"
+          alt=""
+          width="100%"
+        />
+      </div>
+      <div class="col-12 col-sm-6 col-md-8">
+        <h6>
+          S6S Mini GPS Drones 150g Drone 4K Profesional HD Dual Camera 5G WIFI
+          FPV Brushless Folding Quadcopter RC Dron Helicopter Toys
+        </h6>
+        <div
+          class="p-3 mb-2 text-white"
+          style="background: orangered; color: white; font-weight: bold"
+        >
+          Precio: 350 BOB
+        </div>
+        <h5>Color</h5>
+        <div>
+          <div class="color-box clic" style="background: red"></div>
+          <div class="color-box clic" style="background: blue"></div>
+          <div class="color-box clic" style="background: black"></div>
+        </div>
+        <h5>Cantidad</h5>
+        <div class="quantity">
+          <button>-</button>
+          <div>1</div>
+          <button>+</button>
+        </div>
+        <div class="buy-box">
+          <button type="button" class="btn btn-primary">Comprar</button>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
   props: {
-    msg: String
-  }
-}
+    msg: String,
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
+.color-box {
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  margin: 7px;
   display: inline-block;
-  margin: 0 10px;
 }
-a {
-  color: #42b983;
+
+.clic {
+  cursor: pointer;
+}
+
+.quantity button {
+  border-radius: 50%;
+  display: inline-block;
+  width: 30px;
+}
+.quantity div {
+  text-align: center;
+  min-width: 30px;
+  display: inline-block;
+  font-weight: bold;
+}
+.buy-box {
+  margin: 20px;
+}
+.container {
+  margin-top: 50px;
+  text-align: left;
+}
+.producto-relacionado-precio {
+  background: orangered;
+  color: white;
+  text-align: center;
+  padding: 10px;
 }
 </style>
